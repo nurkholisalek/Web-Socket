4287557d3dd3aff7af972b2f821de2fa745b2662
Nur Kholis Alek Anjar Ma'ruf <nurkholisalekanjarmaruf88@gmail.com>
2023-12-22 18:29:29 +0700

    add

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Web-Socket
+
+go 1.21.5
+
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/novalagung/gubrak/v2 v2.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+type M map[string]interface{}
+
+const MESSAGE_NEW_USER = "New User"
+const MESSAGE_CHAT = "Chat"
+const MESSAGE_LEAVE = "Leave"
+
+var connections = make([]*WebSocketConnection, 0)
