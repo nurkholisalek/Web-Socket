3928115fa36dfd639156e1222dd7930ccd1c8352
Nur Kholis Alek Anjar Ma'ruf <nurkholisalekanjarmaruf88@gmail.com>
2023-12-22 21:47:10 +0700

    main.go feature

--- main.go
+++ main.go
@@ -7,3 +7,13 @@ const MESSAGE_CHAT = "Chat"
 const MESSAGE_LEAVE = "Leave"
 
 var connections = make([]*WebSocketConnection, 0)
+
+type SocketPayload struct {
+	Message string
+}
+
+type SocketResponse struct {
+	From    string
+	Type    string
+	Message string
+}
